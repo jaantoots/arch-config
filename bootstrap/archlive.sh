9738f567447c2d040cfa8c67f20bda4d9e589130
Jaan Toots <jaan@jaantoots.org>
2019-10-03 01:00:39 +0300

    Install systemd-networkd DHCP to archlive

--- bootstrap/archlive.sh
+++ bootstrap/archlive.sh
@@ -5,6 +5,7 @@ set -eux
 cp -av /usr/share/archiso/configs/releng/ archlive
 
 install -Dm644 -t archlive/airootfs/etc/pacman.d mirrorlist
+install -Dm644 -t archlive/airootfs/etc/systemd/network 10-dhcp.network
 install -Dm644 -t archlive/airootfs/etc/iwd iwd/main.conf
 install -Dm600 -t archlive/airootfs/var/lib/iwd iwd/*.psk
 install -Dm644 -t archlive/airootfs/root/.ssh authorized_keys
