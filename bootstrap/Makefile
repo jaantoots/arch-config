819a74958f1d6a05f6edd0975bdd5ce99fbe9f4c
Jaan Toots <jaan@jaantoots.org>
2019-10-02 16:45:19 +0300

    Add archlive creation and configuration

--- bootstrap/Makefile
+++ bootstrap/Makefile
@@ -2,13 +2,19 @@ ROOT = https://www.archlinux.org/iso
 MIRROR ?= https://mirror.rackspace.com/archlinux/iso
 RELEASE ?= 2019.10.01
 
+mirrorlist:
+	reflector --verbose --sort rate --score 64 --fastest 16 > $@
+
+archlive: mirrorlist
+	./archlive.sh
+
 .PHONY: image
 image: archlinux-bootstrap-$(RELEASE)-x86_64.tar.gz.sig archlinux-bootstrap-$(RELEASE)-x86_64.tar.gz
 	gpg --verify $<
 
 .PHONY: clean
 clean:
-	rm -rf archlinux-bootstrap-*-x86_64.tar.gz
+	rm -rf archlinux-bootstrap-*-x86_64.tar.gz archlive mirrorlist
 
 archlinux-bootstrap-%-x86_64.tar.gz:
 	wget $(MIRROR)/$*/$@
---
+++ bootstrap/archlive.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eux
+
+cp -av /usr/share/archiso/configs/releng/ archlive
+
+install -Dm644 -t archlive/airootfs/etc/pacman.d mirrorlist
+install -Dm644 -t archlive/airootfs/etc/iwd iwd/main.conf
+install -Dm600 -t archlive/airootfs/var/lib/iwd iwd/*.psk
+install -Dm644 -t archlive/airootfs/root/.ssh authorized_keys
+
+ln -sf ../run/systemd/resolve/stub-resolv.conf archlive/airootfs/etc/resolv.conf
+sed -i -e '/\/etc\/pacman\.d\/mirrorlist/d' \
+    -e 's/ choose-mirror.service/ iwd.service systemd-networkd.service systemd-resolved.service sshd.service/' \
+    archlive/airootfs/root/customize_airootfs.sh
